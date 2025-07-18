4b6cf9096c5b76950b72ad7e8f295918c0159beb
ferrc897 <ferrodriguez8972005@gmail.com>
2025-07-17 20:12:08 -0600

    creación de runner.py

---
+++ runner.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1000, 800))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
